331b637d73b2e7fd1ae85bbc4b5f78f40b0f63c2
Linda Cai <linda.cai@zoho.com>
2019-02-10 15:25:36 -0800

    rearrange functions

--- polly_puns.py
+++ polly_puns.py
@@ -16,9 +16,34 @@ def get_puns():
 
 
 def get_kicks_crashes(path_prefix='sample'):
+    """Load all kick and crash files found in path_prefix;
+    Note that this assumes all matching files will read properly"""
     kicks = [get_soundfile_data_only(fpath) for fpath in glob.glob(os.path.join(path_prefix, 'kick*ogg'))]
     crashes = [get_soundfile_data_only(fpath) for fpath in glob.glob(os.path.join(path_prefix, 'crash*ogg'))]
     return kicks, crashes
+
+
+def get_soundfile_data_only(file_name):
+    data, sr = sf.read(file_name)
+    # handle case of multiple channels
+    if len(data.shape) > 1 and data.shape[1] >= 2:
+        return (data[:,0] + data[:,1]) / 2
+    return data
+
+
+def get_rand_two_kicks_crash(kicks, crashes):
+    """joke happened type sound effect"""
+    two_kicks = random.sample(kicks, 2)
+    one_crash = random.sample(crashes, 1)
+    
+    return np.concatenate(two_kicks + one_crash)
+
+
+def add_sound_effect(file_name, kicks, crashes):
+    """add sound effect to the end of a sound from a file and write back to the same file"""
+    data, sr = sf.read(file_name)
+    data_with_end = np.concatenate([data, get_rand_two_kicks_crash(kicks, crashes)])
+    sf.write(file_name, data_with_end, sr)
 
 
 def convert_to_ssml(string):
@@ -31,19 +56,6 @@ def convert_to_ssml(string):
     ssml =  ssml_template % (unemph, emph_strong)
     print(ssml)
     return ssml
-
-
-def get_rand_two_kicks_crash(kicks, crashes):
-    """joke happened type sound effect"""
-    two_kicks = random.sample(kicks, 2)
-    one_crash = random.sample(crashes, 1)
-    
-    return np.concatenate(two_kicks + one_crash)
-
-
-def save_joke_to_local(joke_name, data):
-    with open(joke_name, 'wb') as file:
-        file.write(data)
 
 
 def get_joke_encoded_binary(polly, joke_text):
@@ -61,18 +73,10 @@ def get_joke_file_name(output_path, joke_text):
     return os.path.join(output_path, joke_name)
 
 
-def get_soundfile_data_only(file_name):
-    data, sr = sf.read(file_name)
-    if len(data.shape) > 1 and data.shape[1] == 2:
-        return (data[0] + data[1])/2
-    return data
-
-
-def add_sound_effect(file_name, kicks, crashes):
-    data, sr = sf.read(file_name)
-    data_with_end = np.concatenate([data, get_rand_two_kicks_crash(kicks, crashes)])
-    sf.write(file_name, data_with_end, sr)
-
+def save_to_local(joke_path, data):
+    """save data to file path"""
+    with open(joke_path, 'wb') as file:
+        file.write(data)
 
 
 def get_parsed_args():
@@ -94,7 +98,10 @@ if __name__ == "__main__":
 
     for pun in puns:
         joke_binary = get_joke_encoded_binary(polly, joke_text=convert_to_ssml(pun))
-        file_name = get_joke_file_name(output_path, pun)
-        save_joke_to_local(file_name, joke_binary)
-        add_sound_effect(file_name, kicks, crashes)
-        print('%s output' % (file_name))
+        joke_path = get_joke_file_name(output_path, pun)
+
+        save_to_local(joke_path, joke_binary)
+
+        # replaces file with file containing sound effects
+        add_sound_effect(joke_path, kicks, crashes)
+        print('%s output' % (joke_path))
